c773252b2f1e9e3638aa4f0b6e1785639af4cab3
Mahesh K Neupane <maheshkneupane90@gmail.com>
2025-04-07 09:58:54 +0545

    fix: add basename to react router for gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename={import.meta.env.BASE_URL}>
           <CustomCursor />
           <Routes>
             <Route path="/" element={<Index />} />
